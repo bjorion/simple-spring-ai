8faea074441c71615f50afe93a85874e8f5c26f9
bjorion <bjorion@gmail.com>
2026-02-03 20:29:45 +0100

    feat(tool): add a tool

--- src/main/java/com/example/boardgamebuddy/tool/ToolController.java
+++ src/main/java/com/example/boardgamebuddy/tool/ToolController.java
@@ -1,6 +1,5 @@
 package com.example.boardgamebuddy.tool;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ToolController {
 
     private static final String CURRENT_TIME_TEMPLATE =
-                "What is the current time in {city}?";
+            "What is the current time in {city}?";
 
     private final ChatClient chatClient;
 
@@ -21,15 +20,13 @@ public class ToolController {
                 .build();
     }
 
-    @GetMapping(path="/time", params = "city")
+    @GetMapping(path = "/time", params = "city")
     public String getTime(@RequestParam("city") String city) {
 
         return chatClient.prompt()
-                .user(userSpec -> {
-                    userSpec
-                            .text(CURRENT_TIME_TEMPLATE)
-                            .param("city", city);
-                })
+                .user(userSpec -> userSpec
+                        .text(CURRENT_TIME_TEMPLATE)
+                        .param("city", city))
                 .call()
                 .content();
     }
